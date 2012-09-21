94c84593a5df3225603699c71d48dd04ace664aa
Mikhail Grushin <mihail.grushin@gmail.com>
2012-09-21 20:56:00 +0400

    View printer class

---
+++ view/printer.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "printer.h"
+
+void Printer::screenClear() {
+	printf("%c[2J", 27);
+	fflush(stdout);
+}
+
+void Printer::screenHome() {
+	printf("%c[H", 27);
+	fflush(stdout);
+}
+
+void Printer::screenToPoint(int i, int j) {
+	printf("%c[%d;%dH", 27, i, j);
+	fflush(stdout);
+}
---
+++ view/printer.h
@@ -0,0 +1,9 @@
+#include <iostream>
+
+class Printer {
+	public:
+	static void screenClear(void);
+	static void screenHome(void);
+	static void screenToPoint(int x, int y);
+	private:
+};
